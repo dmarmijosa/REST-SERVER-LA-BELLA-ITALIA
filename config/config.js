7b8e133211f14ea2df1754ee80e84be01ab6557e
Danny Armijos <daniel2667@hotmail.es>
2022-02-13 13:31:14 +0100

    avance

--- config/config.js
+++ config/config.js
@@ -18,11 +18,17 @@ types.setTypeParser(1114,function(stringValue){
 
 
 const databaseConfig={
-    'host':'localhost',
+    'host':'ec2-52-30-133-191.eu-west-1.compute.amazonaws.com',
     'port':5432,
-    'database':'delivery_db',
-    'user':'postgres',
-    'password': '12251619'
+    'database':'dcjav7dg76hn9h',
+    'user':'nqbcdzbxbooogb',
+    'password': '061c74852e2e79f12dae7215c0f764588a0dde6db43336c46fca9252e24b4b2d',
+    'ssl': true,
+        'extra': {
+        'ssl': {
+            'rejectUnauthorized': false,
+        },
+        },
 };
 
 const db= pgp(databaseConfig);
