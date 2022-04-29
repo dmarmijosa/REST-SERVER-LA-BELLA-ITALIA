fbf06d9ec81535308d55309d9db01c889d588990
Danny Armijos <71116392+dmarmijosa@users.noreply.github.com>
2022-04-29 12:55:32 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -20,9 +20,9 @@ types.setTypeParser(1114,function(stringValue){
 const databaseConfig={
     'host':'ec2-79-125-26-60.eu-west-1.compute.amazonaws.com',
     'port':5432,
-    'database':'dcjo88b2kic6v9',
+    'database':
     'user':'hfladqvescwxxe',
-    'password': '3d8a09ec0fcf7b7e4900e562d89ae567cebc8b86100bec201a97d11c4922ca6b',
+    'password': 
     ssl: { rejectUnauthorized: false }
 };
 */
@@ -31,7 +31,7 @@ const databaseConfig={
     'port':5432,
     'database':'delivery_db',
     'user':'postgres',
-    'password': '12251619'
+    'password': 
 };
 const db= pgp(databaseConfig);
 
